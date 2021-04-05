57f01822ea4ce406253810783012bd1598feb19e
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-04-05 17:52:21 -0400

    Improve cpl_data_prep bin file to be consistent with current
    Reformat it to be easily read
     On branch feature/dev_cpl_p5
    	modified:   bin/wcoss_dell_p35/cpl_data_prep.sh
    
    Refs: #24

--- rocoto/bin/wcoss_dell_p35/cpl_data_prep.sh
+++ rocoto/bin/wcoss_dell_p35/cpl_data_prep.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
 set -x
+ulimit -s unlimited
+ulimit -a
+
+# module_ver.h
+. $SOURCEDIR/versions/gefs_wcoss_dell_p35.ver
+
+# Load modules
+. /usrx/local/prod/lmod/lmod/init/ksh
+module list
+module purge
+
+module load EnvVars/$EnvVars_ver
+module load ips/$ips_ver
+module load impi/$impi_ver
+module load prod_util/$prod_util_ver
+module load prod_envir/$prod_envir_ver
+#module load HDF5-parallel/$HDF5_parallel_ver
+#module load NetCDF-parallel/$NetCDF_parallel_ver
+
+module load lsf/$lsf_ver
+
+#module load CFP/$CFP_ver
+#export USE_CFP=YES
+
+module list
+
+# For Development
+. $GEFS_ROCOTO/bin/wcoss_dell_p35/common.sh
+
+#set -x
 
 ###############################################################
 ## Abstract:
@@ -22,9 +52,11 @@ set -x
 #export BASE_ENV=$HOMEgfs/env
 export CDATE=${PDY}00
 
-. $HOMEgfs/ush/load_fv3gfs_modules.sh
-status=$?
-[[ $status -ne 0 ]] && exit $status
+#. $HOMEgfs/ush/load_fv3gfs_modules.sh
+#status=$?
+#[[ $status -ne 0 ]] && exit $status
+
+#. $GEFS_ROCOTO/bin/wcoss_dell_p35/common.sh
 
 #export DATAROOT="$ROTDIR"
 #[[ ! -d $DATAROOT ]] && mkdir -p $DATAROOT
@@ -65,41 +97,41 @@ wavinitdir=$p5_025icdir/wav
 for mem in $MEMLIST
 do
 
-# Create ICSDIR if needed
-[[ ! -d $ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT ]] && mkdir -p $ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT
-[[ ! -d $ICSDIR/$mem/FV3ICS/ocn ]] && mkdir -p $ICSDIR/$mem/FV3ICS/ocn
-[[ ! -d $ICSDIR/$mem/FV3ICS/ice ]] && mkdir -p $ICSDIR/$mem/FV3ICS/ice
+    # Create ICSDIR if needed
+    [[ ! -d $ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT ]] && mkdir -p $ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT
+    [[ ! -d $ICSDIR/$mem/FV3ICS/ocn ]] && mkdir -p $ICSDIR/$mem/FV3ICS/ocn
+    [[ ! -d $ICSDIR/$mem/FV3ICS/ice ]] && mkdir -p $ICSDIR/$mem/FV3ICS/ice
 
-# Setup ATM initial condition files
-#cp -r $ORIGIN_ROOT/$CPL_ATMIC/$CDATE/$CDUMP  $ICSDIR/$CDATE/
-cp -r $atminitdir/gefs.$PDY/$cyc/$mem/*  $ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT
+    # Setup ATM initial condition files
+    #cp -r $ORIGIN_ROOT/$CPL_ATMIC/$CDATE/$CDUMP  $ICSDIR/$CDATE/
+    cp -r $atminitdir/gefs.$PDY/$cyc/$mem/*  $ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT
 
-# Setup Ocean IC files 
-#cp -r $ORIGIN_ROOT/$CPL_OCNIC/$CDATE/ocn/$OCNRES/MOM*.nc  $ICSDIR/$CDATE/ocn/
-#cp -r $cplinit/$CDATE/050/ocn  $ICSDIR/$mem/FV3ICS
-cp -r $ocninitdir/$PDY/MOM4_TS_restart_regular.nc $ICSDIR/$mem/FV3ICS/ocn/MOM6_IC_TS.nc
-#Setup Ice IC files 
-#cp $ORIGIN_ROOT/$CPL_ICEIC/$CDATE/ice/$ICERES/cice5_model_${ICERESdec}.res_$CDATE.nc $ICSDIR/$CDATE/ice/
-#cp -r $cplinit/$CDATE/050/ice $ICSDIR/$mem/FV3ICS
-cp -r $iceinitdir/cice5_model_0.25.res_${CDATE}.nc $ICSDIR/$mem/FV3ICS/ice/cice5_model_0.25.res_${CDATE}.nc
-if [ $cplwav = ".true." ]; then
-  [[ ! -d $ICSDIR/$mem/FV3ICS/wav ]] && mkdir -p $ICSDIR/$mem/FV3ICS/wav
-#  for grdID in $waveGRD
-#  do
-    #cp $ORIGIN_ROOT/$CPL_WAVIC/$CDATE/wav/$grdID/*restart.$grdID $ICSDIR/$CDATE/wav/
-    cp -r $wavinitdir/${PDY}.000000.restart.gwes_30m $ICSDIR/$mem/FV3ICS/wav
-#echo no wave initial data at this time
-#  done
-fi
+    # Setup Ocean IC files 
+    #cp -r $ORIGIN_ROOT/$CPL_OCNIC/$CDATE/ocn/$OCNRES/MOM*.nc  $ICSDIR/$CDATE/ocn/
+    #cp -r $cplinit/$CDATE/050/ocn  $ICSDIR/$mem/FV3ICS
+    cp -r $ocninitdir/$PDY/MOM4_TS_restart_regular.nc $ICSDIR/$mem/FV3ICS/ocn/MOM6_IC_TS.nc
+    #Setup Ice IC files 
+    #cp $ORIGIN_ROOT/$CPL_ICEIC/$CDATE/ice/$ICERES/cice5_model_${ICERESdec}.res_$CDATE.nc $ICSDIR/$CDATE/ice/
+    #cp -r $cplinit/$CDATE/050/ice $ICSDIR/$mem/FV3ICS
+    cp -r $iceinitdir/cice5_model_0.25.res_${CDATE}.nc $ICSDIR/$mem/FV3ICS/ice/cice5_model_0.25.res_${CDATE}.nc
+    if [ $cplwav = ".true." ]; then
+        [[ ! -d $ICSDIR/$mem/FV3ICS/wav ]] && mkdir -p $ICSDIR/$mem/FV3ICS/wav
+        #  for grdID in $waveGRD
+        #  do
+        #cp $ORIGIN_ROOT/$CPL_WAVIC/$CDATE/wav/$grdID/*restart.$grdID $ICSDIR/$CDATE/wav/
+        cp -r $wavinitdir/${PDY}.000000.restart.gwes_30m $ICSDIR/$mem/FV3ICS/wav
+        #echo no wave initial data at this time
+        #  done
+    fi
 
-export OUTDIR="$ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT"
-mkdir -p $ICSDIR/ocndaily/$mem
-# Stage the FV3 initial conditions to ROTDIR
-memCOMOUT="$ROTDIR/$mem"
-[[ ! -d $memCOMOUT ]] && mkdir -p $memCOMOUT
-cd $memCOMOUT || exit 99
-rm -rf INPUT
-#$NLN $OUTDIR .
+    export OUTDIR="$ICSDIR/$mem/FV3ICS/gfs/$CASE/INPUT"
+    mkdir -p $ICSDIR/ocndaily/$mem
+    # Stage the FV3 initial conditions to ROTDIR
+    memCOMOUT="$ROTDIR/$mem"
+    [[ ! -d $memCOMOUT ]] && mkdir -p $memCOMOUT
+    cd $memCOMOUT || exit 99
+    rm -rf INPUT
+    #$NLN $OUTDIR .
 done
 
 ##############################################################
